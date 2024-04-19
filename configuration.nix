affb7acf03a056d6c53efbbbf2e5cb0ba797c041
Mohammed Bilal N S <70318383+mohammedbilalns@users.noreply.github.com>
2024-04-19 21:51:45 +0530

    nix-thumbdrive first

---
+++ configuration.nix
@@ -0,0 +1,203 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+	imports =
+		[ # Include the results of the hardware scan.
+		./hardware-configuration.nix
+		];
+
+# Bootloader.
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+
+	networking.hostName = "nixos"; # Define your hostname.
+# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+# Configure network proxy if necessary
+# networking.proxy.default = "http://user:password@proxy:port/";
+# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+# Enable networking
+		networking.networkmanager.enable = true;
+
+# Set your time zone.
+	time.timeZone = "Asia/Kolkata";
+
+# Select internationalisation properties.
+	i18n.defaultLocale = "en_IN";
+
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "en_IN";
+		LC_IDENTIFICATION = "en_IN";
+		LC_MEASUREMENT = "en_IN";
+		LC_MONETARY = "en_IN";
+		LC_NAME = "en_IN";
+		LC_NUMERIC = "en_IN";
+		LC_PAPER = "en_IN";
+		LC_TELEPHONE = "en_IN";
+		LC_TIME = "en_IN";
+	};
+
+# Enable the X11 windowing system.
+	services.xserver.enable = true;
+
+# Enable the GNOME Desktop Environment.
+	services.xserver.displayManager.gdm.enable = true;
+	services.xserver.desktopManager.gnome.enable = true;
+
+	environment.gnome.excludePackages = (with pkgs; [
+			gnome-photos
+			gnome-tour
+	]) ++ (with pkgs.gnome; [
+		cheese # webcam tool
+		gnome-music
+		epiphany # web browser
+		geary # email reader 
+		gnome-characters
+		totem # video player
+		tali # poker game
+		iagno # go game
+		hitori # sudoku game
+		atomix # puzzle game
+	]);
+
+
+	programs.zsh.enable = true;
+	users.defaultUserShell = pkgs.zsh;
+	programs.hyprland.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+  # TLP 
+  services.tlp.enable = true;
+  services.blueman.enable = true;
+  services.power-profiles-daemon.enable = false;
+  services.atuin.enable =true;
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.mohammedbilalns = {
+    isNormalUser = true;
+    description = "Mohammed Bilal N S";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+    #  thunderbird
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+    neovim
+    waybar
+    cinnamon.nemo
+    cinnamon.nemo-fileroller 
+    telegram-desktop
+    zathura
+    nwg-look
+    texliveFull
+    neofetch
+    htop
+    bat
+    yazi
+    qbittorrent
+    rnote
+    papirus-icon-theme
+    github-desktop
+    tlp
+    joplin-desktop
+    wofi
+    hyprshade 
+    hyprpaper
+    fira-code-nerdfont
+    qt6Packages.qt6ct
+    qt6Packages.qtstyleplugin-kvantum
+    brightnessctl 
+    pamixer
+    mako
+    blueman
+    cliphist
+    zsh
+    rocmPackages.llvm.clang
+    gcc
+    tmux
+    cl
+    zig
+    tree-sitter
+    nodejs_21
+    cargo
+    foot
+    orchis-theme
+    killall
+    networkmanagerapplet
+    
+    
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+  nix.settings.experimental-features =[ "nix-command" "flakes" ];
+
+
+}
+
